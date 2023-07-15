dc15406deeb6030b4fac9dc48254ab4530a06170
ElviraMadamovich <110635987+ElviraMadamovich@users.noreply.github.com>
2023-07-15 21:56:08 +0300

    корректировка кода

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 require('dotenv').config();
-<<<<<<< Updated upstream
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
-=======
-const { errors } = require('celebrate');
-const express = require('express');
-const mongoose = require('mongoose');
->>>>>>> Stashed changes
+const cors = require('./middlewares/cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errorHandler');
 const routes = require('./routes/index');
-<<<<<<< Updated upstream
-const { DB_URL } = require('./utils/constants');
-=======
->>>>>>> Stashed changes
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
@@ -27,50 +17,14 @@ const limiter = rateLimit({
 
 const app = express();
 
-<<<<<<< Updated upstream
-const { PORT = 3000, NODE_ENV, DB_URL } = process.env;
-mongoose.connect(NODE_ENV === 'production' ? DB_PATH : DB_URL);
-
-const allowedCors = [
-  'https://elviram.movies-explorer.nomoredomains.work',
-  'http://elviram.movies-explorer.nomoredomains.work',
-];
-
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const allowedMethods = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-  res.header('Access-Control-Allow-Credentials', 'true');
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', allowedMethods);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-
-    return res.end();
-  }
-
-  return next();
-});
-
-app.use(cors());
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-=======
 const { PORT = 3000 } = process.env;
-
-const cors = require('./middlewares/cors');
 
 mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors);
->>>>>>> Stashed changes
 app.use(helmet());
+app.use(cors);
 app.use(requestLogger);
 app.use(limiter);
 
@@ -78,16 +32,6 @@ app.use(routes);
 
 app.use(errorLogger);
 
-<<<<<<< Updated upstream
 app.use(errorHandler);
 
 app.listen(PORT);
-=======
-app.use(errors());
-
-app.use(errorHandler);
-
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
->>>>>>> Stashed changes
